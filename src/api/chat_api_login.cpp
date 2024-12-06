825c4149a4e4b4a105944567f32e57e2d2e239e3
chr56 <chr0056@gmail.com>
2024-12-06 20:21:00 +0800

    [api] return user id

--- src/api/chat_api_login.cpp
+++ src/api/chat_api_login.cpp
@@ -18,8 +18,15 @@ ChatApiService::Login(grpc::CallbackServerContext *context, const UserCredential
         reactor->Finish(grpc::Status(grpc::StatusCode::UNAUTHENTICATED, "Invalid credentials"));
         return reactor;
     } else {
-        absl::PrintF("User %s logged in\n", name.value());
-        result->set_user_id(100); // todo
+        std::string &user_name = name.value();
+        if (auto user = userManager.get_user_by_name(user_name)) {
+            auto id = user.value()->id();
+            result->set_user_id(id);
+            absl::PrintF("User %s (%u) logged in\n", user_name, id);
+        } else {
+            result->set_user_id(0);
+            absl::PrintF("Could not find user %s? \n", user_name);
+        }
         reactor->Finish(grpc::Status::OK);
         return reactor;
     }
