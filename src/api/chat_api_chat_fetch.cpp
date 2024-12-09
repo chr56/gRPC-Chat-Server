76d8e46ac3beb9f01448b6b63f059f1439eb7474
chr56 <chr0056@gmail.com>
2024-12-09 09:54:44 +0800

    [api] add all users as chat member

--- src/api/chat_api_chat_fetch.cpp
+++ src/api/chat_api_chat_fetch.cpp
@@ -9,15 +9,23 @@ using namespace api::chat;
 class ChatApiService::ChatListReactor : public grpc::ServerUnaryReactor {
 public:
     ChatListReactor(
-            grpc::CallbackServerContext *context, ChatList *reply, ChatManager chatManager
+            grpc::CallbackServerContext *context, ChatList *reply, ChatManager chatManager, UserManager userManager
     ) : _context(context) {
         const std::list<Chat> &allChats = chatManager.list_all_chats();
-        for (const auto& chat: allChats) {
+        for (const auto &chat: allChats) {
             Chat *newChat = pending.add_chats();
             newChat->set_id(chat.id());
             newChat->set_name(chat.name());
             newChat->set_is_group(chat.is_group());
             newChat->set_description(chat.description());
+            auto members = newChat->mutable_members();
+            const auto &users = userManager.list_all_users();
+            for (auto item: users) { // todo
+                User *user = members->add_users();
+                user->set_id(item->id());
+                user->set_name(item->name());
+                user->set_description(item->description());
+            }
         }
         reply->Swap(&pending);
         Finish(grpc::Status::OK);
@@ -54,7 +62,7 @@ ChatApiService::FetchChatList(grpc::CallbackServerContext *context, const None *
     }
     absl::PrintF("Sending Chat list to %s \n", name.value());
 
-    reactor = new ChatApiService::ChatListReactor(context, list, chatManager);
+    reactor = new ChatApiService::ChatListReactor(context, list, chatManager, userManager);
 
     return reactor;
 }
